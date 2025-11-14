ecc0fc09ebbb437642bb7e2c58ee5b4b1678f878
Nagapandi <nagapandi1998>
2025-11-14 17:23:17 +0530

    bug fixed

--- src/app/pages/all-cases/all-cases.ts
+++ src/app/pages/all-cases/all-cases.ts
@@ -20,7 +20,6 @@ import { Loader } from '../../shared/components/loader/loader';
     MatPaginatorModule,
     MatButtonModule,
     MatCardModule,
-    Loader,
   ],
   templateUrl: './all-cases.html',
   styleUrl: './all-cases.scss',
